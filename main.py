08cf8030fcece37a099199e0bf6186de7ec272a6
chaim <chchdeu@gmail.com>
2026-07-27 16:54:33 +0300

    added_validations

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from config import player_settigs, monster_settigs
 from player import Player
 from monster import Monster
 from battle import Battle
+from validations import Validations
 
-player = Player(player_settigs["name"], player_settigs["health"], player_settigs["strength"], player_settigs["agility"], player_settigs["luck"], player_settigs["attack"])
-monster = Monster(monster_settigs["name"], monster_settigs["health"], monster_settigs["strength"], monster_settigs["agility"], monster_settigs["luck"], monster_settigs["attack"])
-battle = Battle(player, monster)
-battle.run()
+
+if Validations.validtae_config(player_settigs,monster_settigs):
+    player = Player(player_settigs["name"], player_settigs["health"], player_settigs["strength"], player_settigs["agility"], player_settigs["luck"], player_settigs["attack"])
+    monster = Monster(monster_settigs["name"], monster_settigs["health"], monster_settigs["strength"], monster_settigs["agility"], monster_settigs["luck"], monster_settigs["attack"])
+
+    battle = Battle(player, monster)
+
+
+    battle.run()
+else:
+    print ("something is wrong, game exiting")
