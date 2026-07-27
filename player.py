1e2f5c5a939f3e97f708581a3ed00f948deb6f75
chaim <chchdeu@gmail.com>
2026-07-27 15:10:32 +0300

    edit_player

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
-from entity import entity
+from entity import  Entity
 
-class player(entity):
+class player(Entity):
     def __init__(self, name, health, strength, agility, luck):
         super().__init__(name, health, strength, agility, luck)
