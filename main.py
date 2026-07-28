b3213d0451557e611bb281b5d050a88753cf8b40
yaakov-ai <ysoibelman@gmail.com>
2026-07-28 16:33:56 +0300

    changed a bit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ try:
     from config import player_settings, monster_settings
 except ImportError:
     print("The config file is missing one or more of the variables player_settings, monster_settings")
+    print ("Fix and run game again")
     exit()
 from player import Player
 from monster import Monster
@@ -9,10 +10,12 @@ from battle import Battle
 from validations import Validations
 
 if Validations.key_in_dict(player_settings) and (monster_settings):
-    if Validations.val_key_value(player_settings,monster_settings):
+    if Validations.valgit_key_value(player_settings,monster_settings):
         player = Player(player_settings["name"], player_settings["health"], player_settings["strength"], player_settings["agility"], player_settings["luck"], player_settings["attack"])
         monster = Monster(monster_settings["name"], monster_settings["health"], monster_settings["strength"], monster_settings["agility"], monster_settings["luck"], monster_settings["attack"])
         battle = Battle(player, monster)
         battle.run()
     else:
         print ("Fix and run game again")
+else:
+    print ("Fix and run game again")
