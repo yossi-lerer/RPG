a3f847f6c1acc1a25477bb195a5e534d627ffa01
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-28 16:42:46 +0300

    fix the vaild condtion

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from monster import Monster
 from battle import Battle
 from validations import Validations
 
-if Validations.key_in_dict(player_settings) and (monster_settings):
+if Validations.key_in_dict(player_settings) and Validations.key_in_dict(monster_settings):
     if Validations.val_key_value(player_settings,monster_settings):
         player = Player(player_settings["name"], player_settings["health"], player_settings["strength"], player_settings["agility"], player_settings["luck"], player_settings["attack"])
         monster = Monster(monster_settings["name"], monster_settings["health"], monster_settings["strength"], monster_settings["agility"], monster_settings["luck"], monster_settings["attack"])
