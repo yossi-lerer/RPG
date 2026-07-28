d96ac4953d29424a2a021c5eb0380e2dc7b44e12
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-28 16:11:16 +0300

    fix the config file

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-player_settings = {"name": "mosh", "health": 80.9, "strength": 50.5, "agility": 5, "luck": 5, "attack": 20}
-monster_settings = {"name": "mon", "health": 80.9, "strength": 50.5, "agility": 5, "luck": 5, "attack": 30}
-if "name" in player_settings:
-    print ("Hello")
+player_settigs = {"name": "mosh", "health": 80.9, "strength": 50.5, "agility": 5, "luck": 5, "attack": 20}
+monster_settigs = {"name": "mosh", "health": 80.9, "strength": 50.5, "agility": 5, "luck": 5, "attack": 20}
