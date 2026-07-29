04b0eb973ba9dd9f349e33524497b92177cf0124
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-29 10:57:08 +0300

    add two method to player

--- player.py
+++ player.py
@@ -3,3 +3,10 @@ from entity import  Entity
 class Player(Entity):
     def __init__(self, name, health, strength, agility, luck, attack):
         super().__init__(name, health, strength, agility, luck, attack)
+    
+    def can_take_potion(self):
+        return True
+    
+    def add_potion(self):
+        print("Added a potion to the bag")
+        return True
