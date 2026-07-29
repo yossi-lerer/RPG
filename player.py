c4871e19c08fcb9b51cede6c71bceae3dc4ed784
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-29 11:05:56 +0300

    add logic

--- player.py
+++ player.py
@@ -3,10 +3,14 @@ from entity import  Entity
 class Player(Entity):
     def __init__(self, name, health, strength, agility, luck, attack):
         super().__init__(name, health, strength, agility, luck, attack)
+        self.potion = 0
     
     def can_take_potion(self):
-        return True
+        if self.potion < 2:
+            return True
+        else:
+            return False
+
     
     def add_potion(self):
-        print("Added a potion to the bag")
-        return True
+        self.potion += 1
