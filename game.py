bd4ed5b017da01d36baaf0a78f6da34e049988ce
chaim <chchdeu@gmail.com>
2026-07-29 11:20:32 +0300

    added_basic_game

---
+++ game.py
@@ -0,0 +1,51 @@
+from player import *
+from room import *
+from battle import *
+
+class Game:
+    def __init__(self,player,maze):
+        self.player=player
+        self.maze=maze
+        self.current_room_index=0
+
+    def run(self):
+        while self.player.is_alive() and self.current_room_index < self.maze.room_count:
+            choice = self.show_main_menu()
+            if choice == "1":
+                self.enter_next_room()
+            elif choice == "2":
+                self.show_summary()
+                break
+
+        if not self.player.is_alive():
+            self.show_summary()
+        elif self.current_room_index == self.maze.room_count:
+            self.show_summary()
+        
+    def show_main_menu(self):
+        user_choose=input("choose an option \n 1. enter next room \n 2. leave game")
+        return user_choose
+
+    def enter_next_room(self):
+        room = self.maze.get_room(self.current_room_index)
+        if room.has_monster():
+            battle = Battle(self.player, room.monster)
+            battle.run()
+
+        elif room.has_potion():
+            if self.player.add_potion():
+                room.take_potion()
+                self.add_potion()
+        self.current_room_index += 1
+
+    def show_summary(self):
+        knight = "\U0001F3C7"
+        print(f"""{self.player.name} {knight} stats are;
+Life: {self.player.health}
+Strength: {self.player.strength}
+Agility: {self.player.agility}
+Luck: {self.player.luck}""")
+
+
+        
+
